5f4e6bc100c032f22a9988ff8a6dc35d2a8d83a2
saymu_c <saymonhenrique2374@gmail.com>
2025-04-17 18:48:33 -0400

    quase pronto
    base feita, so falta polir e corrigir alguns bugs

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ const MENU = [
     'Sair'
 ];
 
+function limpar() {
+    console.clear();
+}
+limpar();
+
 let cliente = [];
 
 const user = readline.question("Digite seu nome: ");
-console.log(`Olá ${user}, Bem vindo ao sistemas!`);
+limpar();
+console.log(`Olá ${user}, Bem vindo ao sistema!`);
 
 function carregarDados(nomeArquivo) {
   if (fs.existsSync(nomeArquivo)) {
@@ -23,7 +29,6 @@ function carregarDados(nomeArquivo) {
   }
   return [];
 }
-
 function salvarDados(nomeArquivo, novosDados) {
   const dadosAtuais = carregarDados(nomeArquivo);
   const dadosAtualizados = [...dadosAtuais, ...novosDados];
@@ -65,6 +70,7 @@ function Menus() {
         }
     })
     .catch((error) => {
+        limpar();
         console.error(chalk.red('Ocorreu um erro:', error.message || error));
         Menus();
     });
@@ -79,37 +85,43 @@ function verificarECarregarJSON(caminhoArquivo) {
       if (conteudo !== '') {
         try {
             const dadus = JSON.parse(conteudo)
-            console.log('Aqui esta todos os clientes:');
             console.log(JSON.stringify(dadus, null, 2));
           return Menus();
         } catch (erro) {
-          console.log(chalk.red('❌ Erro ao interpretar o JSON:', erro.message));
+            limpar();
+          console.log(chalk.red('Erro ao interpretar o JSON:', erro.message));
           return Menus(); 
         }
       } else {
-        console.log(chalk.red('⚠️ Arquivo existe, mas está vazio.'));
+        limpar();
+        console.log(chalk.red('Arquivo existe, mas está vazio.'));
         return Menus();
       }
     } else {
-      console.log(chalk.red('⚠️ Arquivo não encontrado.'));
+        limpar();
+      console.log(chalk.red('Arquivo não encontrado.'));
       return Menus();
     }
   }
 
 function cadastrarCliente() {
+    limpar();
     console.log('===cadastro de clientes===');
     const nome_cliente = readline.question('Nome do Cliente: ').trim();
     if (nome_cliente === '') {
+        limpar();
         console.log(chalk.red('O nome não pode ser vazio!'));
         return Menus();
     }
     const email_cliente = readline.questionEMail('Email do Cliente: ').trim();
     const numero_cliente = readline.questionInt('Numero do Cliente: ');
     if (email_cliente === '') {
+        limpar();
         console.log(chalk.red('O email não pode ser vazio!'));
         Menus();
     }
     if (isNaN(numero_cliente) || numero_cliente <= 0) {
+        limpar();
         console.log('Número inválido, digite um número válido!');
         Menus();
     }
@@ -120,13 +132,95 @@ function cadastrarCliente() {
         Numero_Cliente: numero_cliente
     });
     salvarDados('clientes.json', clientesExistentes);
+    limpar();
     console.log(chalk.green('Cliente cadastrado com sucesso!'))
     Menus();
 }
 
 function listarClientes() {
+    limpar();
     console.log('===Clientes cadastrados===\n')
     verificarECarregarJSON('clientes.json')
 };
+
+function buscarCliente() {
+    limpar();
+    console.log('===Buscar Clientes===');
+    const buscar_nome = readline.question('Nome que deseja buscar: ').trim();
+
+    let nomes_existentes = [];
+    if (fs.existsSync('clientes.json')) {
+        const dados = fs.readFileSync('clientes.json', 'utf8');
+        if (dados.trim() !== '') {
+            nomes_existentes = JSON.parse(dados);
+        }
+    }
+    const nomes_filtrados = nomes_existentes.filter((CLIENTE) => 
+        CLIENTE.Nome_Cliente.toLowerCase().includes(buscar_nome.toLowerCase())
+    );
+
+    if (buscar_nome === '') {
+        limpar();
+        console.log(chalk.red('Digite um nome válido.\n'));
+        return Menus();
+    } else if (nomes_filtrados.length === 0) {
+        limpar();
+        console.log(chalk.red(`Nenhum Cliente cadastrado com o nome ${buscar_nome}\n`));
+    } else {
+        limpar();
+        console.log(chalk.green(`Clientes encontrados com o nome ${buscar_nome}: \n`));
+    nomes_filtrados.forEach((CLIENTE, index) => {
+        console.log(`${index + 1}. ${chalk.yellow(CLIENTE.Nome_Cliente)} \nEmail: ${CLIENTE.Email_Cliente} \nNúmero: ${CLIENTE.Numero_Cliente}\n`)
+    });
+    }
+    Menus();
+
+}
+
+function removerCliente() {
+    limpar();
+    let clientesExistentes = carregarDados('clientes.json');
+    console.log('=== Remover Clientes ===');
+
+    if (clientesExistentes.length === 0) {
+        limpar();
+        console.log(chalk.red('Não há nenhum cliente cadastrado!'));
+        return Menus();
+    }
+
+    inquirer
+        .prompt([
+            {
+                name: 'remover',
+                type: 'list',
+                message: 'Escolha qual cliente você deseja remover:',
+                choices: clientesExistentes.map((cliente) => cliente.Nome_Cliente),
+            }
+        ])
+        .then((answers) => {
+            const clienteExiste = clientesExistentes.some(
+                (cliente) => cliente.Nome_Cliente === answers.remover
+            );
+
+            if (clienteExiste) {
+                clientesExistentes = clientesExistentes.filter(
+                    (cliente) => cliente.Nome_Cliente !== answers.remover
+                );
+                limpar();
+                fs.writeFileSync('clientes.json', JSON.stringify(clientesExistentes, null, 2), 'utf8');
+                console.log(chalk.green(`Cliente "${answers.remover}" removido com sucesso!`));
+            } else {
+                limpar();
+                console.log(chalk.red('Cliente não encontrado!'));
+            }
+
+            Menus();
+        })
+        .catch((error) => {
+            limpar();
+            console.error(chalk.red('Ocorreu um erro:'), error.message || error);
+            Menus();
+        });
+}
 
 Menus();
